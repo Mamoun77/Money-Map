2d52aa037ef7e9cefb0b64c4e87d12e4773e4e47
Mamoun77 <mamounhussam1000@gmail.com>
2025-12-31 16:52:00 +0300

    Add the requirements file for deployment

--- app/routes.py
+++ app/routes.py
@@ -1271,4 +1271,5 @@ def check_goal_achievements(user_id):
     
     db.session.commit()
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
